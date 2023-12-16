8b535195541b63be9789e70be8dcbd9011d1ec6a
LadyDi3103 <chinamy2004@gmail.com>
2023-12-16 16:41:33 -0500

    ADD: LOGGIN validation

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -4,30 +4,30 @@ import { FormControl, FormsModule } from '@angular/forms';
 import { HeaderComponent } from '../../shared/shared-components/header/header.component';
 import FooterComponent from 'src/app/shared/shared-components/footer/footer.component';
 import { LoginService } from 'src/app/services/login/login.service';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+
   public email: string = '';
   public emailError: string = '';
-  password: string = '';
+  public password: string = '';
   public passwordError: string = '';
 
-  constructor(private router: Router , public loginService: LoginService) {}
+  constructor(private fb: FormBuilder,private router: Router , public loginService: LoginService) {
 
-  onLoginClick() {
-    // Simula la verificación del correo y contraseña (reemplaza con tu lógica real)
-    if (this.email === 'juan@doctor.pe') {
-      // Autenticación exitosa para el administrador, redirigir a home-admin
-      this.router.navigate(['dashboard']);
-    } else {
-      // Autenticación fallida, muestra un mensaje de error
-      this.emailError = 'Por favor, ingrese un correo válido';
-    }
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]]
+    });
+
+
   }
+
 
    
     // ngOnInit() {
@@ -35,8 +35,18 @@ export class LoginComponent {
     //     console.log('RES 34', data);
     //   })
     // }
+    
     onSubmit(){
+      // debugger
+       // Aquí puedes manejar la lógica de autenticación
+    if (this.loginForm.valid && this.loginForm.value.email === 'juan@doctor.pe') {
       this.router.navigateByUrl('/dashboard');
+      console.log('Formulario válido. Enviar datos al servidor.');
+    } else {
+      console.log('Formulario inválido. Verifica tus datos.');
+      this.emailError = 'Por favor, ingrese un correo válido';
+    }
+ 
     }
   }
   
