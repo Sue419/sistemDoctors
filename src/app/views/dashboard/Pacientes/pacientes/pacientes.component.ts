73f6472d7066098c5b158a4fcc6baec7910a9daf
LadyDi3103 <chinamy2004@gmail.com>
2023-12-16 02:37:49 -0500

    UPDATE: LOGIN

--- src/app/views/dashboard/Pacientes/pacientes/pacientes.component.ts
+++ src/app/views/dashboard/Pacientes/pacientes/pacientes.component.ts
@@ -12,7 +12,7 @@ import {LiveAnnouncer} from '@angular/cdk/a11y';
 })
 export class PacientesComponent implements AfterViewInit{
 
-  displayedColumns: string[] = ['position', 'nombrePaciente', 'dni', 'symbol'];
+  displayedColumns: string[] = ['position', 'nombrePaciente', 'dni', 'acciones'];
   dataSource = new MatTableDataSource<PacientesList>(ELEMENT_DATA);
 
   @ViewChild(MatSort) sort!: MatSort;
@@ -44,17 +44,17 @@ export interface PacientesList {
   nombrePaciente: string;
   position: number;
   dni: number;
-  symbol: string;
+  acciones: string;
 }
 const ELEMENT_DATA: PacientesList[] = [
-  {position: 1, nombrePaciente: 'Laura Gomez', dni: 44165393, symbol: 'H'},
-  {position: 2, nombrePaciente: 'Miranda Checa', dni: 11111111, symbol: 'He'},
-  {position: 3, nombrePaciente: 'Lupe Acosta', dni: 22222222, symbol: 'Li'},
-  {position: 4, nombrePaciente: 'Mariana Portugal', dni: 33333333, symbol: 'Be'},
-  {position: 5, nombrePaciente: 'Juana Revoredo', dni: 15555530, symbol: 'B'},
-  {position: 6, nombrePaciente: 'Lucas Rubinshtein', dni: 15478963, symbol: 'C'},
-  {position: 7, nombrePaciente: 'Nadia Quispe', dni: 98745632, symbol: 'N'},
-  {position: 8, nombrePaciente: 'Sebastian Osorio', dni: 99999999, symbol: 'O'},
-  {position: 9, nombrePaciente: 'María Toribio', dni: 55555555, symbol: 'F'},
-  {position: 10, nombrePaciente: 'Azucena Quispe', dni: 66666666, symbol: 'Ne'},
+  {position: 1, nombrePaciente: 'Laura Gomez', dni: 44165393, acciones: ''},
+  {position: 2, nombrePaciente: 'Miranda Checa', dni: 11111111, acciones: ''},
+  {position: 3, nombrePaciente: 'Lupe Acosta', dni: 22222222, acciones: ''},
+  {position: 4, nombrePaciente: 'Mariana Portugal', dni: 33333333, acciones: ''},
+  {position: 5, nombrePaciente: 'Juana Revoredo', dni: 15555530, acciones: ''},
+  {position: 6, nombrePaciente: 'Lucas Rubinshtein', dni: 15478963, acciones: ''},
+  {position: 7, nombrePaciente: 'Nadia Quispe', dni: 98745632, acciones: ''},
+  {position: 8, nombrePaciente: 'Sebastian Osorio', dni: 99999999, acciones: ''},
+  {position: 9, nombrePaciente: 'María Toribio', dni: 55555555, acciones: ''},
+  {position: 10, nombrePaciente: 'Azucena Quispe', dni: 66666666, acciones: ''},
 ];
