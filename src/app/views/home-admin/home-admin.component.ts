431548c5394904e3fe73f56179feed5cc70d25b4
LadyDi3103 <chinamy2004@gmail.com>
2023-12-17 16:57:36 -0500

    ADD: home-view

--- src/app/views/home-admin/home-admin.component.ts
+++ src/app/views/home-admin/home-admin.component.ts
@@ -7,5 +7,27 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./home-admin.component.css']
 })
 export default class HomeAdminComponent {
+  servicios = [
+    {
+      titulo: 'BOTOX',
+      imagen: '../../../assets/servicios/descarga.png',
+      descripcion: 'Descripción del Servicio 1'
+    },
+    {
+      titulo: 'FILLERS',
+      imagen: '../../../assets/servicios/filler.jpg',
+      descripcion: 'Descripción del Servicio 2'
+    },
+    {
+      titulo: 'RINO-RELLENO',
+      imagen: '../../../assets/servicios/rino.avif',
+      descripcion: 'Descripción del Servicio 3'
+    }
+  ];
+
+
+
+
+
 
 }
