f81048d2db264f0bc153178c58a0c9cc1ce33994
LadyDi3103 <chinamy2004@gmail.com>
2023-12-19 16:50:41 -0500

    ADD: ENVIRONMENT SWITCH

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
-  production: true,
-  apiKey: 'pk.eyJ1IjoibGFkeWRpMzEwMyIsImEiOiJjbHBnaXNtOTEwODN6Mm1wOWxzNDJ1dmFoIn0.FaJVOmTtVnQXVv5r8EHsPw',
   BASE_URL_BACK: 'https://sis-doctors-back.onrender.com/',
-  URL_ENDPOINT_MEDICOS:'/medicos'
+  URL_ENDPOINT_MEDICOS:'/medicos',
+  URL_ENDPOINT_PACIENTES:'/pacientes'
 };
